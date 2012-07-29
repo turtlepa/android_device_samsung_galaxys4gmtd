73bcfef36a105d87ecd5347867d50710d6bb4bd6
Bryan Hundven <bryanhundven@gmail.com>
2012-07-29 04:36:03 -0700

    Initial push based on captivatemtd and vibrantmtd.
    Change-Id: I2309a21ab27639f42e29fc0cfa648d9b7210eddf

---
+++ BoardConfig.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# Kernel Config
+TARGET_KERNEL_CONFIG := cyanogenmod_galaxys4gmtd_defconfig
+
+TARGET_OTA_ASSERT_DEVICE := galaxys4g,galaxys4gmtd,SGH-T959V,SGH-T959W
+
+# Import the aries-common BoardConfigCommon.mk
+include device/samsung/aries-common/BoardConfigCommon.mk
+
+# Override some partition size weirdness
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 7864320
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 403701760
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 555745280
+BOARD_FLASH_BLOCK_SIZE := 4096
