089a15858c28a939cc70b9921ed9c60872b92f15
daothanhduy <daothanhduy2806@gmail.com>
2013-12-31 19:49:21 +0700

    Added back the custom graphic.c in order to have the charger screen works even tho TWRP doesn't use this as well as the battery level may works strangely.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/l
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USES_BML_OVER_MTD := true
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/galaxys4gmtd/shbootimg.mk
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/aries-common/recovery/graphics.c
 
 # TWRP
 DEVICE_RESOLUTION := 480x800
