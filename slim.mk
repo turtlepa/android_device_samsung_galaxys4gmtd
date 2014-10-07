2e02cface32902ab152025208029ffa456b34871
David Lamontagne <DJLaMontagneIII@gmail.com>
2014-10-07 02:26:23 -0400

    Update for SlimKat
    Change-Id: I015ab3dc4a1ca7ee7da054083567458ebb3f061e

--- slim.mk
+++ slim.mk
@@ -1,20 +1,28 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyS4G
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/galaxys4gmtd/full_galaxys4gmtd.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.res=5MP
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := galaxys4gmtd
-PRODUCT_NAME := cm_galaxys4gmtd
+PRODUCT_NAME := slim_galaxys4gmtd
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-T959V
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T959V TARGET_DEVICE=SGH-T959V BUILD_FINGERPRINT=samsung/SGH-T959V/SGH-T959V:2.3.6/GINGERBREAD/VUVKJ6:user/release-keys PRIVATE_BUILD_DESC="SGH-T959V-user 2.3.6 GINGERBREAD VUVKJ6 release-keys"
+
